e285a0441f0c33b62443445f68fcc093b62148bc
J-grigri <jurgis.grigorjevs@gmail.com>
2020-03-17 22:15:24 +0700

    v2

--- src/components/ChoiseCard.js
+++ src/components/ChoiseCard.js
@@ -1,13 +1,26 @@
 import React from 'react' ;
 import "../App.css";
+import { getRoundOutcome } from '../App';
 
 
 export default function ChoiseCard(props) {
+    console.log(props)
+    const getOutcome = (title, winner) => {
+    // this function will return 2 values, first is the string we want to show , 2nd is the class name
+        if (winner === "tie") {
+            return ["Tie","tie"]//1st is variable, 2nd is CSS class
+        } else if (title === winner) {
+            return ["Win", "winner"] 
+        } else return ["Lose","loser"]
+    };
+    // lets try a tie
+    // will have more logic here to check who wins who loses
+    console.log(getOutcome(props.title, props.winner)) // forgot to pass arguments
     return (
-        <div className={`choice-card ${props.winner ? 'winner' : 'loser'}`}>
+        <div className={`choice-card ${getOutcome(props.title, props.winner)[1]}`}>
             <h1>{props.title}</h1>
             <img src={props.imgURL} alt={props.title} />
-            <h3>{props.winner ? 'Won' : 'Loss'}</h3>
+            <h3>{getOutcome(props.title,props.winner)[0]}</h3> 
         </div>
     )
 }
